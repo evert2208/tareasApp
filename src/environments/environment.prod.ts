d9d883473e7e3cbd07c711934ba1373753756765
evert2208 <65311634+evert2208@users.noreply.github.com>
2025-11-26 14:08:23 -0500

    apk e ipa relase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+  apiKey: "AIzaSyDfsXjwRuHjwF68gHSLTLwGPddvIvJhN2Q",
+  authDomain: "ionic-task-47b5d.firebaseapp.com",
+  projectId: "ionic-task-47b5d",
+  storageBucket: "ionic-task-47b5d.firebasestorage.app",
+  messagingSenderId: "746566738616",
+  appId: "1:746566738616:web:77bdf9c69c4b9e387bca4d"
+  }
 };
